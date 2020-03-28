02ff5c54bd61fdd243540235e82192208eed491b
kiritka-jain <58281851+kiritka-jain@users.noreply.github.com>
2020-03-28 20:55:55 +0530

    Add basic run functionality to the game

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ def print_grid(grid: List) -> None:
     :param grid: Grid to be printed
     :return:  None
     """
+    # TODO: beautify print
     for row in grid:
         for column in row:
             print(column, end='')
@@ -17,4 +18,32 @@ def print_grid(grid: List) -> None:
 grid = [['.', '.', '.'],
         ['.', '.', '.'],
         ['.', '.', '.']]
-print_grid(grid)
+
+
+def terminate_game(chance: int) -> bool:
+    """
+    check if game needs to be terminated or not
+    :param chance: no. of chances played in the game
+    :return: bool
+    """
+    if chance > 9:
+        return True
+    return False
+
+chance = 0
+while True:
+    chance += 1
+    # TODO: Check game status
+    if terminate_game(chance):
+        break
+    print_grid(grid)
+    print('---------------------------------------------------------')
+    if chance % 2 == 0:
+        print('Player 2 to play\n')
+        i,j = map(int,input().split())
+        grid[i-1][j-1] = 'X'
+    else:
+        print('Player 1 to play\n')
+        i,j = map(int,input().split())
+        grid[i-1][j-1] = 'O'
+
